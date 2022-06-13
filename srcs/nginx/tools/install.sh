7cc454a5c7741ef54c7abb57af257594a5818a3e
bcarlee <bcarlee@student.21-school.ru>
2022-06-13 17:03:36 +0300

    nginx dockerfile

--- srcs/nginx/tools/install.sh
+++ srcs/nginx/tools/install.sh
@@ -1,7 +1,7 @@
-if [ ! -f /etc/ssl/certs/nginx.crt ]; then
+sudo apt-get update
+sudo apt-get -y install nginx wget
 openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
 		-keyout /etc/ssl/private/nginx.key \
 		-out /etc/ssl/certs/nginx.crt \
 		-subj "/C=RU/ST=Tatarstan/L=Kazan/O=wordpress/CN=bcarlee.42.fr";
-fi
-nginx -g 'daemon off;';
+mv nginx.conf /etc/nginx/sites-available/default
