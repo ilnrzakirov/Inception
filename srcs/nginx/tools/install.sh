d8f38747ed3286361349fa1765bd6babb1edd12f
bcarlee <bcarlee@student.21-school.ru>
2022-06-13 16:58:10 +0300

    nginx dockerfile

---
+++ srcs/nginx/tools/install.sh
@@ -0,0 +1,8 @@
+if [ ! -f /etc/ssl/certs/nginx.crt ]; then
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
+		-keyout /etc/ssl/private/nginx.key \
+		-out /etc/ssl/certs/nginx.crt \
+		-subj "/C=RU/ST=Tatarstan/L=Kazan/O=wordpress/CN=bcarlee.42.fr";
+echo "Nginx: ssl is set up!";
+fi
+nginx -g 'daemon off;';
