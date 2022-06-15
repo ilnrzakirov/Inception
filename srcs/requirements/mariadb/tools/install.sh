3b7d4fbe56af8b9761db1ab2c7b6d6d56c876617
bcarlee <bcarlee@student.21-school.ru>
2022-06-15 15:29:33 +0300

    nginx dockerfile

--- srcs/requirements/mariadb/tools/install.sh
+++ srcs/requirements/mariadb/tools/install.sh
@@ -1,3 +1,4 @@
 apt-get update
 apt-get -y install mariadb-server
+mkdir ~/data/db
 mv 50-server.conf /etc/mysql/mariadb.conf.d/
--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,4 +1,5 @@
 if ! [ -d "/var/www/wordpress/wp-config.php" ]; then
+    mkdir ~/data/wp
     wp core config --dbhost=${DB_HOST} --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --allow-root
     wp core install --url=${DOMAIN_NAME} --title=${TITLE} --admin_user=${USER_NAME} --admin_password=${DB_ROOT_PASSWORD} --admin_email=${USER_ADMIN_EMAIL} --skip-email --allow-root
     wp user create ${DB_USER} ${USER_EMAIL} --allow-root --role=subscriber --user_pass=${DB_PASSWORD}
