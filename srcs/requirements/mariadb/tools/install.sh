bacda48418f28ad5bb386fbbdb2af0848f21fd5e
bcarlee <bcarlee@student.21-school.ru>
2022-06-15 16:35:55 +0300

    nginx dockerfile

--- srcs/requirements/mariadb/tools/install.sh
+++ srcs/requirements/mariadb/tools/install.sh
@@ -1,4 +1,5 @@
 apt-get update
-apt-get -y install mariadb-server
-mkdir ~/data/db
+apt-get install -y mariadb-server
 mv 50-server.conf /etc/mysql/mariadb.conf.d/
+sed -i 's!#port!port !g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i 's!127.0.0.1!0.0.0.0!g' /etc/mysql/mariadb.conf.d/50-server.cnf
